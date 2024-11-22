482fa6f12416cc95668d8c8af5d5de1f2f2d5ce3
Ian Hwang <ian807b@gmail.com>
2024-11-21 20:49:42 -0800

    Add navigation and authentication screens with React Navigation setup

--- MatzipApp/front/App.tsx
+++ MatzipApp/front/App.tsx
@@ -1,46 +1,13 @@
+import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-  Button,
-  TextInput,
-  SafeAreaView,
-} from 'react-native';
+import RootNavigator from './src/navigations/root/RootNavigator';
 
 function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.input} />
-        <Text>텍스트</Text>
-      </View>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.input} />
-        <Text>텍스트</Text>
-      </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <RootNavigator />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'yellow',
-  },
-  input: {
-    flex: 1,
-    borderWidth: 2,
-    borderColor: 'black',
-    height: 100,
-    width: 100,
-  },
-  inputContainer: {
-    flex: 1,
-    backgroundColor: 'red',
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-});
 
 export default App;
