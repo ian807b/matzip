b85b4fabd4aa449bdd3a440e1058f160901221db
Ian Hwang <ian807b@gmail.com>
2025-01-13 21:49:10 -0800

    added compound component

--- MatzipApp/front/src/screens/feed/FeedDetailScreen.tsx
+++ MatzipApp/front/src/screens/feed/FeedDetailScreen.tsx
@@ -30,6 +30,8 @@ import {CompositeScreenProps} from '@react-navigation/native';
 import {DrawerScreenProps} from '@react-navigation/drawer';
 import {MainDrawerParamList} from '@/navigations/drawer/MainDrawerNavigator';
 import useLocationStore from '@/store/useLocationStore';
+import useModal from '@/hooks/useModal';
+import FeedDetailOption from '@/components/feed/FeedDetailOption';
 
 type FeedDetailScreenProps = CompositeScreenProps<
   StackScreenProps<FeedStackParamList, typeof feedNavigations.FEED_DETAIL>,
@@ -41,6 +43,7 @@ function FeedDetailScreen({route, navigation}: FeedDetailScreenProps) {
   const {data: post, isPending, isError} = useGetPost(id);
   const insets = useSafeAreaInsets();
   const {setMoveLocation} = useLocationStore();
+  const detailOption = useModal();
 
   if (isPending || isError) {
     return <></>;
@@ -71,7 +74,12 @@ function FeedDetailScreen({route, navigation}: FeedDetailScreenProps) {
               color={colors.WHITE}
               onPress={() => navigation.goBack()}
             />
-            <Ionicons name="ellipsis-vertical" size={30} color={colors.WHITE} />
+            <Ionicons
+              name="ellipsis-vertical"
+              size={30}
+              color={colors.WHITE}
+              onPress={detailOption.show}
+            />
           </View>
         </SafeAreaView>
 
@@ -163,6 +171,11 @@ function FeedDetailScreen({route, navigation}: FeedDetailScreenProps) {
           />
         </View>
       </View>
+
+      <FeedDetailOption
+        isVisible={detailOption.isVisible}
+        hideOption={detailOption.hide}
+      />
     </>
   );
 }
